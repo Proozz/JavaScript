79dbe69656776636e651cb3a659f8cece19c63b2
Proozz <marcusvls20081@gmail.com>
2024-02-24 19:16:54 +0000

    hehe

---
+++ aula - 11/Ex007.js
@@ -0,0 +1,8 @@
+
+var vel = 60
+
+console.log(`a velocidade do seu carro ${vel}km/h`)
+
+if (vel > 60){console.log('voce ultrapassou a velocidade permitida. Multado!')} //condição simples
+
+console.log('dirija sempre usando cinto de segurança')
