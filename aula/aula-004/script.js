674ce9d47f47a65044d5d30367bdbaaf42d02d83
Proozz <marcusvls20081@gmail.com>
2024-02-25 19:30:59 +0000

    hehe

---
+++ aula/aula-004/script.js
@@ -0,0 +1,3 @@
+window.alert("bom dia :)")       //janela com aviso (alerta)
+window.confirm("gosta de pao?")  //janela com confirmação
+window.prompt("se sim, qual?")   //janela com texto para resposta
---
+++ aula/aula-009/script.js
@@ -0,0 +1,11 @@
+var corpo = document.body
+var p1 = document.getElementById('p')
+  p1.style.color = "blue"
+  p1.style.backgroundColor = "aqua"
+
+var d = document.querySelector('p#p1')
+
+  d.style.color = 'green'
+  d.style.backgroundColor = "black"
+
+window.alert(d.innerHTML)
